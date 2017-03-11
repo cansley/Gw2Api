52c60312fb0d5c66d6e939327a070a11b2b70e83
Charles Ansley <cansley2@gmail.com>
2017-03-11 00:18:41 -0600

    added starting code to display inventory by character.

--- app/Controllers/characterController.js
+++ app/Controllers/characterController.js
@@ -5,8 +5,8 @@ app.config(['$routeProvider', function($routeProvider) {
     });
 }])
 
-app.controller("characters", ['$scope', 'config', 'gwApiUrlHelper', 'gw2ApiUrls', '$http',
-    function characters($scope, config, gwApiUrlHelper, gw2ApiUrls, $http){
+app.controller("characters", ['$scope', 'config', 'gwApiUrlHelper', 'gw2ApiUrls',
+    function characters($scope, config, gwApiUrlHelper, gw2ApiUrls){
     $scope.config = config;
     $scope.GetCharacters = function(){
         if ($scope.characters !== undefined && $scope.characters.length > 0) return;
@@ -28,6 +28,7 @@ app.controller("characters", ['$scope', 'config', 'gwApiUrlHelper', 'gw2ApiUrls'
         response.then(function(value){
                 $scope.selectedCharacter = value.data;
                 console.log($scope.selectedCharacter);
+                $scope.GetInventory();
             },
             function(error){console.log(error);});
     };
@@ -35,5 +36,29 @@ app.controller("characters", ['$scope', 'config', 'gwApiUrlHelper', 'gw2ApiUrls'
     $scope.prettyPrint = function (obj) {
         var str = JSON.stringify(obj, undefined, 4);
         return str;
+    }
+
+    $scope.GetInventory = function(){
+        var char = $scope.selectedCharacter;
+        var bags = char.bags;
+        $scope.InventoryItems = [];
+        bags.forEach(function(bag){
+            var inventory = bag.inventory;
+            inventory.forEach(function(item){
+                if(item !== null) {
+                    var itemId = item.id;
+                    var itemInfo = gwApiUrlHelper.GetContentFromAreaWithVal(gw2ApiUrls.Items, itemId);
+                    itemInfo.then(function (value) {
+                        var thisItem = value.data;
+                        thisItem.Quantity = item.count;
+                        if(thisItem.details != undefined && thisItem.details.charges != undefined){
+                            thisItem.Quantity = thisItem.details.charges;
+                        }
+                        $scope.InventoryItems.push(thisItem);
+                        console.log(value.data);
+                    });
+                }
+            });
+        });
     }
 }]);
--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ var app = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+    'angular.filter'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
